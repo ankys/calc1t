ff0c62aaf26a4eb4d4d2ed6a7e470683bc92b109
ankys <ankysp@gmail.com>
2025-12-20 18:12:37 +0000

    move fonts setup

---
+++ .setup/fonts.sh
@@ -0,0 +1,13 @@
+
+sudo mkdir -p /usr/share/fonts/
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-Regular.otf' -o /usr/share/fonts/SourceSans3-Regular.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-It.otf' -o /usr/share/fonts/SourceSans3-It.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-It.otf' -o /usr/share/fonts/SourceSans3-It.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-It.otf' -o /usr/share/fonts/SourceSans3-It.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-It.otf' -o /usr/share/fonts/SourceSans3-It.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-It.otf' -o /usr/share/fonts/SourceSans3-It.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-Bold.otf' -o /usr/share/fonts/SourceSans3-Bold.otf
+sudo curl 'https://raw.githubusercontent.com/adobe-fonts/source-sans/refs/heads/release/OTF/SourceSans3-BoldIt.otf' -o /usr/share/fonts/SourceSans3-BoldIt.otf
+sudo curl 'https://raw.githubusercontent.com/trueroad/HaranoAjiFonts/master/HaranoAjiMincho-Regular.otf' -o /usr/share/fonts/HaranoAjiMincho-Regular.otf
+sudo curl 'https://raw.githubusercontent.com/trueroad/HaranoAjiFonts/master/HaranoAjiGothic-Regular.otf' -o /usr/share/fonts/HaranoAjiGothic-Regular.otf
+sudo curl 'https://raw.githubusercontent.com/trueroad/HaranoAjiFonts/master/HaranoAjiGothic-Bold.otf' -o /usr/share/fonts/HaranoAjiGothic-Bold.otf
